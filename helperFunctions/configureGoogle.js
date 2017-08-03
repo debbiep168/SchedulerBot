6cc904e6314db311ee167462f234d2984f56ad8c
DebbiePao <dpao@berkeley.edu>
2017-08-02 21:18:21 -0700

    fixing

--- helperFunctions/configureGoogle.js
+++ helperFunctions/configureGoogle.js
@@ -3,7 +3,7 @@ var google = require('googleapis');
 var plus = google.plus('v1');
 var OAuth2 = google.auth.OAuth2;
 
-fuction oauth2Client() {
+function oauth2Client() {
   return new OAuth2 (
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
