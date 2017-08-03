9ec17b437e92ef5c11e2c577ca91591a658b2371
DebbiePao <dpao@berkeley.edu>
2017-08-02 21:55:32 -0700

    fix

--- helperFunctions/findTimeConflicts.js
+++ helperFunctions/findTimeConflicts.js
@@ -27,7 +27,7 @@ function findTimeConflicts(invitees, date, time) {
         return null;
       }
       else {
-        console.log("EVENTSSSSS", events.items);
+        console.log("EVENTSSSSS", events);
         return true;
       }
     })
